3d351568adc2ac78260ecf6d212f3c7e958dd336
Luigi C. Filho <67793599+luigicfilho@users.noreply.github.com>
2025-07-08 12:45:39 -0300

    add some options to disable paralelims between tests

--- LCSoft.Results.Tests/AssemblyInfo.cs
+++ LCSoft.Results.Tests/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 ﻿using Xunit;
 
+#if NET5_0 || NET7_0
 [assembly: CollectionBehavior(DisableTestParallelization = true)]
+#else
+[assembly: CollectionBehavior(DisableTestParallelization = false)]
+#endif
